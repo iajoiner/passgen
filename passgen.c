e2fbe8c0717c7db8253bf9072dd6462c4a36f435
Ying Zhou <yingzhou474@gmail.com>
2017-11-29 15:42:10 -0500

    Fix a bug.

--- passgen.c
+++ passgen.c
@@ -17,14 +17,20 @@ int main(int argc, char** argv)
 		mbedtls_md5_self_test(1);
 	}
 	mbedtls_md5_twisted(argv[1], strlen(argv[1]), twisted_md5sum);
-	count = twisted_md5sum[12] + twisted_md5sum[14];
+	count = twisted_md5sum[12] + twisted_md5sum[14] + twisted_md5sum[15];
 	for (j = 0 ; j < 32; j++) {
 		product[j] = twisted_md5sum[31-j];
 	}
 	for (i = 0; i < count; i++) {
 		mbedtls_md5_twisted(product, 32, product);
 		for (j = 0 ; j < 32; j++) {
-			product[j] = product[31-j];
+			product[j]++;
+			if (product[j] == 91)
+				product[j] = 65;
+			if (product[j] == 123)
+				product[j] = 48;
+			if (product[j] == 58)
+				product[j] = 97;
 		}
 	}
 	for (i = 0; i < 32; i++) {
